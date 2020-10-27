3f8275c2ce4b14d864c6d0104b7c732a78cc9e4a
Danil <danil1gordeev@yandex.ru>
2020-10-27 20:13:10 +0300

    second commit

--- src/main/java/part1/lesson4/task3/Main.java
+++ src/main/java/part1/lesson4/task3/Main.java
@@ -4,6 +4,6 @@ package part1.lesson4.task3;
 
 public class Main {
     public static void main(String[] args)   {
-
+        System.out.println("Hello world!");
     }
 }
---
+++ src/main/java/part1/lesson8/task1/Main.java
@@ -0,0 +1,7 @@
+package part1.lesson8.task1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
